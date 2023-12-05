710fb2ef529b2a1f972be93ea96c693774f691e5
Dennis Clausen <dennis.clausen@bbz-rd-eck.de>
2023-12-05 19:01:41 +0100

    updated service-worker

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["helper.js","63443ddd40be59a3afdf9318fd380986"],["icon.ico","458304c2d4b7bac06d6949ddeaff304b"],["icon.png","2ef56805e55c50ca1f3fb9abf142e68d"],["index.html","d86946a5eb296eedd13bf364712cc053"],["manifest.json","2db55f544c309bfb416853ef0cb57a8a"],["start.html","abcc641759e6052b2090383da40e45cd"],["teachr.html","2c7fa083199fdb2b280fb6ce7124f178"]];
+var precacheConfig = [["helper.js","63443ddd40be59a3afdf9318fd380986"],["icon.ico","458304c2d4b7bac06d6949ddeaff304b"],["icon.png","2ef56805e55c50ca1f3fb9abf142e68d"],["index.html","2869ec698c885aa8f3a62267e3d52e8b"],["manifest.json","2db55f544c309bfb416853ef0cb57a8a"],["start.html","abcc641759e6052b2090383da40e45cd"],["teachr.html","9ebab4b84f10f32c662085bb416753bd"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
